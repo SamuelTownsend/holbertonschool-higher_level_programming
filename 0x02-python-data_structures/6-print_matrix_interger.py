77509e0539f31e5389ddf166ad19b2a50088a961
SamuelTownsend <samjaytee13@gmail.com>
2022-05-05 12:14:20 -0700

    initial commits for first half of project

---
+++ 0x02-python-data_structures/6-print_matrix_interger.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def print_matrix_interrger(matrix=[[]]):
+    for x in matrix:
+        count = 0
+        for y in x:
+            if count < len(x) - 1:
+                print("{:d}".format(y), end =" ")
+                else:
+                    print("{:d}".format(y), end=" ")
+                    count = count + 1
+                    print()
