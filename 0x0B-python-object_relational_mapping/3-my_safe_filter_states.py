5ccfd48e212b3c8f8656aaa9353b66228591e130
SamuelTownsend <samjaytee13@gmail.com>
2022-06-23 16:47:03 -0700

    3 update

--- 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0B-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+"""
+Script displays all values in given database where name matches the argument
+"""
+import MySQLdb
+
+
+if __name__ == "__main__":
+    from sys import argv
+    dbconnect = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                             passwd=argv[2], database=argv[3])
+
+    cursor = dbconnect.cursor()
+
+    cursor.execute("""
+        SELECT * FROM states
+        WHERE name LIKE BINARY '{}'
+        ORDER BY states.id ASC
+        """.format(argv[4].split()[0]))
+
+    for row in cursor.fetchall():
+        print(row)
+
+    cursor.close()
+    dbconnect.close()
