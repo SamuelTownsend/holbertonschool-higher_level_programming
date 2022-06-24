e3adc7180e6d396ec16dd8e84323c6328e77ef99
SamuelTownsend <samjaytee13@gmail.com>
2022-06-23 18:11:57 -0700

    new update

--- 0x0B-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0B-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,1 +1,24 @@
 #!/usr/bin/python3
+"""
+script changes the name of the to given where id is a certain number
+"""
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+
+if __name__ == "__main__":
+    dbengine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                             .format(argv[1], argv[2], argv[3]),
+                             pool_pre_ping=True)
+
+    Base.metadata.create_all(dbengine)
+    dbsess = sessionmaker(bind=dbengine)
+    dbs = dbsess()
+
+    upstate = dbs.query(State).filter(State.id == 2).one()
+    upstate.name = "New Mexico"
+    dbs.commit()
+
+    dbsess().close
