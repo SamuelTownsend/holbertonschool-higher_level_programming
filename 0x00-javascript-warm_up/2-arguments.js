86c10ce58e439058ba4f66642705012f41add2aa
SamuelTownsend <samjaytee13@gmail.com>
2022-08-02 14:57:30 -0700

    formatting error fix

--- 0x00-javascript-warm_up/2-arguments.js
+++ 0x00-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 const args = process.argv;
 const len = args.length;
 if (len == 2) {
-    console.log('No argument');
+  console.log('No argument');
 } else if (len == 3) {
-    console.log('Argument found');
+  console.log('Argument found');
 } else {
-    console.log('Arguments found');
+  console.log('Arguments found');
 }
